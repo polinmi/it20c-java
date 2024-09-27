41a508255aabb7854d09a6c611d8e1ed9732033e
polinmi <jeonmay7@gmail.com>
2024-09-27 22:21:22 +0800

    Added Arraylist Package and added 2 classes namely; ItemManager to add a constructor to initialize the Arraylist and on the other hand, Main class to create an object to access all the method on the Item Manager Class

---
+++ src/Arraylist/ItemManager.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Arraylist;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author HP
+ */
+public class ItemManager {
+    
+    private ArrayList<String> items;
+    
+    //Constructor to initialize the ArrayList
+    public ItemManager() {
+        items = new ArrayList<>();
+    }
+}
---
+++ src/Arraylist/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Arraylist;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        //Creating object of the class to access all the methods on the Item Manager
+        ItemManager list = new ItemManager();
+    }
+}
