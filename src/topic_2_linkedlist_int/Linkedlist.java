48edd0ac984fbb80766981575f42688e73c26586
polinmi <jeonmay7@gmail.com>
2024-10-04 12:51:19 +0800

    Added Delete Method

--- src/topic_2_linkedlist_int/Linkedlist.java
+++ src/topic_2_linkedlist_int/Linkedlist.java
@@ -37,6 +37,27 @@ public class Linkedlist {
             System.out.println("null");
         }
 
-     
+      // Method to delete a node by value
+        public void deleteByValue(int value) {
+            if (head == null) return;
+
+            // If the head needs to be removed
+            if (head.data == value) {
+                head = head.next;
+                return;
+            } else if(head.data != value) {      
+
+            }
+
+            Node current = head;
+            while (current.next != null) {
+                if (current.next.data == value) {
+                    current.next = current.next.next; 
+                    return;
+                }
+                current = current.next;
+            }
+        }
+
 }
 
