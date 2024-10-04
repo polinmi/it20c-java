ccdc36d0f25870c640c890d3a07938ef5313ecc4
polinmi <jeonmay7@gmail.com>
2024-10-04 12:49:54 +0800

    Added an instance of the Linkedlist Class to access the methods and Import a scanner to accept input from the user

---
+++ src/topic_2_linkedlist_int/Main.java
@@ -0,0 +1,33 @@
+
+package topic_2_linkedlist_int;
+import java.util.Scanner;
+/**
+ *
+ * @author ADMIN
+ */
+public class Main {
+    
+     public static void main(String[] args) {
+
+        //Creating an object to access the LinkedList Class
+        Linkedlist list = new Linkedlist();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello pipolssss , WELCOME ");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+
+        while(true) {
+
+             System.out.println("Kindly choose a number in the menu you want to do: "
+                + " 1. Add an Item"
+                + " 2. Delete an Item"
+                + " 3. Move/Swap an Item"
+                + " 4. Exit");
+
+            int choice = sc.nextInt();
+
+         
+        }
+    }
+}
---
+++ src/topic_2_linkedlist_int/Node.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package topic_2_linkedlist_int;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Node {
+    
+  //Initalizing variable data and Node next
+     int data;  
+     Node next;  
+
+     // Constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
