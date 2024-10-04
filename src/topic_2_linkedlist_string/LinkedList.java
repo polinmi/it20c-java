793faaa7382a38974a7e7425049df53f92f03a9f
polinmi <jeonmay7@gmail.com>
2024-10-04 14:02:57 +0800

    Added switch code for user's menu choice in main class

--- src/topic_2_linkedlist_string/LinkedList.java
+++ src/topic_2_linkedlist_string/LinkedList.java
@@ -26,6 +26,37 @@ public class LinkedList {
             }
             current.next = newNode;
         }
+        // Method to print the linked list
+        public void printList() {
+            Node current = head;
+            while (current != null) {
+                System.out.print(current.data + " -> ");
+                current = current.next;
+            }
+            System.out.println("null");
+        }
+
+      // Method to delete a node by value
+        public void deleteByValue(String value) {
+            if (head == null) return;
+
+            // If the head needs to be removed
+            if (head.data.equals(value)) {
+                head = head.next;
+                return;
+            } else if(head.data != value) {      
+
+            }
+
+            Node current = head;
+            while (current.next != null) {
+                if (current.next.data.equals(value)) {
+                    current.next = current.next.next; 
+                    return;
+                }
+                current = current.next;
+            }
+        }
 }
 
        
--- src/topic_2_linkedlist_string/Main.java
+++ src/topic_2_linkedlist_string/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package topic_2_linkedlist_string;
+
 import java.util.Scanner;
 
 /**
@@ -12,8 +13,7 @@ import java.util.Scanner;
  */
 public class Main {
 
-
-         public static void main(String[] args) {
+    public static void main(String[] args) {
 
         //Creating an object to access the LinkedList Class
         LinkedList list = new LinkedList();
@@ -32,9 +32,62 @@ public class Main {
                     + " 4. Exit");
 
             int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    // Adding elements to the list
+                    System.out.println("Enter 5 Elements that you want to add: ");
+                    String item1 = sc.next();
+                    String item2 = sc.next();
+                    String item3 = sc.next();
+                    String item4 = sc.next();
+                    String item5 = sc.next();
 
+                    list.add(item1);
+                    list.add(item2);
+                    list.add(item3);
+                    list.add(item4);
+                    list.add(item5);
 
+                    System.out.println("Current Linked List:");
+                    list.printList();
+                    break;
+
+                case 2:
+                    //Delete
+                    System.out.println();
+                    System.out.println("Enter a node that you want to delete: ");
+                    String item6 = sc.next();
+
+                    System.out.println("Deleting an item from the list...");
+                    System.out.println("Current Linked List: ");
+
+                    list.deleteByValue(item6);
+                    list.printList();
+                    break;
+
+                case 3:
+                    //Swap Items
+                    System.out.println();
+                    System.out.println("Enter an index that you want to move: ");
+                    int item7 = sc.nextInt();
+                    System.out.println("Enter an index that you want to swap: ");
+                    int item8 = sc.nextInt();
+                    list.printList();
+
+                    System.out.println("Swapping node from index " + item7 + " to " + item8);
+                    list.swapNodes(item7, item8);
+                    list.printList();
+                    break;
+
+                case 4:
+                    //Exit the program
+                    System.out.println("Thank you! Comeback and run it again.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+            }
+
+        }
     }
-
-}
 }
