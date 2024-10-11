bee19bfc0aef307a085a06358b29e564ac33b6ff
polinmi <jeonmay7@gmail.com>
2024-10-11 22:41:55 +0800

    Added peek method to view the top element of the stack

--- src/topic_3_Stack/Stack.java
+++ src/topic_3_Stack/Stack.java
@@ -41,6 +41,14 @@ public class Stack {
         stackArray[++top] = value;
     }
     
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot peek.");
+            return -1; 
+        }
+        return stackArray[top];
+    }
+    
     
     }
 
