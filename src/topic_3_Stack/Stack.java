d8383a7af24aa773eebb2222e9cb24d5e131ef8d
polinmi <jeonmay7@gmail.com>
2024-10-11 22:38:21 +0800

    Added empty methods in stack

--- src/topic_3_Stack/Stack.java
+++ src/topic_3_Stack/Stack.java
@@ -25,37 +25,8 @@ public class Stack {
         return (top == -1);
     }
 
-    public boolean isFull() {
-        return (top == maxSize - 1);
+   
     }
 
-    public int size() {
-        return top + 1;
-    }
-    
-    public void push(int value) {
-        if (isFull()) {
-            System.out.println("Stack is full. Cannot push " + value);
-            return;
-        }
-        stackArray[++top] = value;
-    }
-    
-    public int peek() {
-        if (isEmpty()) {
-            System.out.println("Stack is empty. Cannot peek.");
-            return -1; 
-        }
-        return stackArray[top];
-    }
-    
-    public int pop() {
-        if (isEmpty()) {
-            System.out.println("Stack is empty. Cannot pop.");
-            return -1;
-        }
-        return stackArray[top--];
-    }
 
-}
 
