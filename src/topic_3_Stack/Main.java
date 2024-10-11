7b24901ca0781b281397e84561e70cc67e14d165
polinmi <jeonmay7@gmail.com>
2024-10-11 22:35:58 +0800

    Added in main  method call that prints text to the console the stack,pop ,size and peek

--- src/topic_3_Stack/Main.java
+++ src/topic_3_Stack/Main.java
@@ -14,6 +14,23 @@ public class Main {
     public static void main(String[] args) {
 		Stack stack = new Stack(5);
 
+        stack.push(50);
+        stack.push(70);
+        stack.push(80);
+        
+        System.out.println("Stack size is: " + stack.size());
+        System.out.println("Top element is: " + stack.peek());
+        System.out.println();
+        
+        //Use case
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Top element after pop: " + stack.peek());
+        System.out.println("Stack size after pop: " + stack.size());
+        
+        System.out.println();
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Popped element: " + stack.pop());
 	}
 
 }
