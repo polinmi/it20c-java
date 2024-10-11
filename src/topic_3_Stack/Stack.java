fc7b21b69381faa05dadd69d9a71a3a2ad42e6f2
polinmi <jeonmay7@gmail.com>
2024-10-11 22:42:41 +0800

    Added pop method to remove and return the top element of the stack with underflow handling

--- src/topic_3_Stack/Stack.java
+++ src/topic_3_Stack/Stack.java
@@ -49,8 +49,13 @@ public class Stack {
         return stackArray[top];
     }
     
-    
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot pop.");
+            return -1;
+        }
+        return stackArray[top--];
     }
 
-
+}
 
