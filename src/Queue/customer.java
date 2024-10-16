8e7820b11a929aedad7a614a0bc44b33e2b15c8e
polinmi <jeonmay7@gmail.com>
2024-10-16 22:57:49 +0800

    Added Main of queue

---
+++ src/Queue/customer.java
@@ -0,0 +1,13 @@
+package Queue;
+
+public class customer {
+    private String name;
+
+    public customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
