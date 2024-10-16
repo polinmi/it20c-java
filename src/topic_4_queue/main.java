986e6d8076f3f29d3aecc72be9e3022d6faf40b5
polinmi <jeonmay7@gmail.com>
2024-10-16 23:04:52 +0800

    Added main in queue

--- src/topic_4_queue/main.java
+++ src/topic_4_queue/main.java
@@ -1,10 +1,17 @@
-package Queue;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+package topic_4_queue;
 import java.util.Scanner;
-
-
+/**
+ *
+ * @author ADMIN
+ */
 public class main {
-    public static void main(String[] args) {
+      public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         queue customerQueue = new queue();
 
@@ -47,3 +54,4 @@ public class main {
         }
     }
 }
+
