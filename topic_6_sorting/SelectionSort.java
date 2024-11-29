17f0691ff5f89333a446d658961c9cdf95f43520
polinmi <jeonmay7@gmail.com>
2024-11-29 09:56:46 +0800

    Added ArrayList for SelectionSort

--- topic_6_sorting/SelectionSort.java
+++ topic_6_sorting/SelectionSort.java
@@ -20,7 +20,14 @@ public class SelectionSort {
         }
     }
 
-    
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+  
 
    
 }
