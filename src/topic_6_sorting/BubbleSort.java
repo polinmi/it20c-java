e5b5a50cd73bc740450670bb13dc6db4a5ea23aa
polinmi <jeonmay7@gmail.com>
2024-12-02 23:35:23 +0800

    Final BubbbleSorting

--- src/topic_6_sorting/BubbleSort.java
+++ src/topic_6_sorting/BubbleSort.java
@@ -1,10 +1,21 @@
+package topic_6_sorting;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class BubbleSort {
 
     public static void bubbleSort(ArrayList<Integer> arr) {
-        // BubbleSort method remains unchanged
+        int n = arr.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr.get(j) > arr.get(j + 1)) {
+                    int temp = arr.get(j);
+                    arr.set(j, arr.get(j + 1));
+                    arr.set(j + 1, temp);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -12,14 +23,21 @@ public class BubbleSort {
         ArrayList<Integer> NumsToSort = new ArrayList<Integer>();
 
         System.out.println("Bubble Sorting ");
-        System.out.println("How many numbers you want to sort?");
+        System.out.println("How many numbers do you want to sort?");
         int nums = scan.nextInt();
-        scan.nextLine(); // consume the newline character
 
         for (int i = 1; i <= nums; i++) {
-            System.out.println("Enter the number you want to sort");
+            System.out.println("Enter number " + i + " to sort:");
             int num = scan.nextInt();
             NumsToSort.add(num);
         }
+
+        System.out.println("Original ArrayList:");
+        System.out.println(NumsToSort);
+
+        bubbleSort(NumsToSort);
+
+        System.out.println("Sorted ArrayList:");
+        System.out.println(NumsToSort);
     }
 }
