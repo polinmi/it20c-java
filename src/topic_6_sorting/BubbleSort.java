d8c767be1d05c09db6db500dfd6756cb77c7c851
polinmi <jeonmay7@gmail.com>
2024-12-02 23:31:40 +0800

    Added user input for number of integers to sort

--- src/topic_6_sorting/BubbleSort.java
+++ src/topic_6_sorting/BubbleSort.java
@@ -1,19 +1,25 @@
-package topic_6_sorting;
-
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class BubbleSort {
- 
+
     public static void bubbleSort(ArrayList<Integer> arr) {
-        int n = arr.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr.get(j) > arr.get(j + 1)) {
-                    int temp = arr.get(j);
-                    arr.set(j, arr.get(j + 1));
-                    arr.set(j + 1, temp);
-                }
-            }
+        // BubbleSort method remains unchanged
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> NumsToSort = new ArrayList<Integer>();
+
+        System.out.println("Bubble Sorting ");
+        System.out.println("How many numbers you want to sort?");
+        int nums = scan.nextInt();
+        scan.nextLine(); // consume the newline character
+
+        for (int i = 1; i <= nums; i++) {
+            System.out.println("Enter the number you want to sort");
+            int num = scan.nextInt();
+            NumsToSort.add(num);
         }
     }
 }
