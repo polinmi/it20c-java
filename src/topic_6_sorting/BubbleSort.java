93483644340ec2b9d527c721b2c0daae5533de24
polinmi <jeonmay7@gmail.com>
2024-12-02 23:20:36 +0800

    Added implemented BubbleSort algorithm using ArrayList to sort numbers

--- src/topic_6_sorting/BubbleSort.java
+++ src/topic_6_sorting/BubbleSort.java
@@ -1,38 +1,19 @@
-
 package topic_6_sorting;
-import java.util.Scanner;
+
+import java.util.ArrayList;
 
 public class BubbleSort {
-      public static void bubbleSort(int[] arr) {
-        int n = arr.length;
+ 
+    public static void bubbleSort(ArrayList<Integer> arr) {
+        int n = arr.size();
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap arr[j] and arr[j + 1]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                if (arr.get(j) > arr.get(j + 1)) {
+                    int temp = arr.get(j);
+                    arr.set(j, arr.get(j + 1));
+                    arr.set(j + 1, temp);
                 }
             }
         }
     }
-
-    public static void printArray(int[] arr) {
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {64, 34, 25, 12, 22, 11, 90};
-        System.out.println("Original arrays:");
-        printArray(arr);
-
-        bubbleSort(arr);
-
-        System.out.println("Sorted arrays:");
-        printArray(arr);
-    }
 }
-
